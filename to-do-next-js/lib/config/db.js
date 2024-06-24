ab66ff595d665896584a594ae963e09ff980fc40
riya461 <riyasabures@gmail.com>
2024-06-24 13:46:51 +0530

    mongo fix

--- to-do-next-js/lib/config/db.js
+++ to-do-next-js/lib/config/db.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose"
 
 export const ConnectDB = async () => {
-    await mongoose.connect('mongodb+srv://meshareo:123456_2003@cluster0.s95xmzc.mongodb.net/');
+    await mongoose.connect('mongodb+srv://meshareo:<password>@cluster0.s95xmzc.mongodb.net/');
     console.log("DB Connected")
 }
